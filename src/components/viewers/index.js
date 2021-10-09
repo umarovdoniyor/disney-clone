d8d2400d7f7a64bc801bec5cb816d3e83c33a718
Doniyor Umarov <doniyorumarov@Doniyors-MacBook-Pro.local>
2021-10-10 03:03:10 +0900

    ADD: Viewers

--- src/components/viewers/index.js
+++ src/components/viewers/index.js
@@ -1,7 +1,25 @@
-import { Container } from './viewers-styles';
+import { Container, Wrap } from './viewers-styles';
 
 function Viewers() {
-  return <div>Viewers</div>;
+  return (
+    <Container>
+      <Wrap>
+        <img src='/images/viewers-disney.png' alt='Viewers-1' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-marvel.png' alt='Viewers-2' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-national.png' alt='Viewers-3' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-pixar.png' alt='Viewers-4' />
+      </Wrap>
+      <Wrap>
+        <img src='/images/viewers-starwars.png' alt='Viewers-5' />
+      </Wrap>
+    </Container>
+  );
 }
 
 export default Viewers;
